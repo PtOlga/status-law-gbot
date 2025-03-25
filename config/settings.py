423855029b7801297d43e751861809d9e6470f93
PtOlga <5441700@gmail.com>
2025-03-25 18:57:49 +0100

    Отладочная информация

--- config/settings.py
+++ config/settings.py
@@ -4,6 +4,11 @@ from dotenv import load_dotenv
 # Загрузка переменных окружения
 load_dotenv()
 
+# Отладочная информация
+print("Текущая директория:", os.getcwd())
+print("Путь к .env:", os.path.join(os.getcwd(), '.env'))
+print("Все переменные окружения:", {k: v for k, v in os.environ.items() if 'TOKEN' in k})
+
 # Пути к директориям
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 VECTOR_STORE_PATH = os.path.join(BASE_DIR, "data", "vector_store")
